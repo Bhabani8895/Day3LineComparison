308d20d1cd366f681d96e9f3823bcc23a710d378
[Bhabani8895] <[bhabaniprof2495@gmail.com]>
2022-03-11 01:00:22 +0530

    Length calculated by cartesian system

--- LineComparision/Program.cs
+++ LineComparision/Program.cs
@@ -7,6 +7,21 @@ namespace LineComparision
        public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Computation Program");
+            Console.Write("Enter any  value x1: ");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter any  value x2: ");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter any  value y1: ");
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter any  value y2: ");
+            double y2 = Convert.ToDouble(Console.ReadLine());
+
+
+            double distance = Convert.ToDouble(Console.Read());
+            //using distance formula to find out distance
+            distance = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+            Console.WriteLine(distance);
+
         }
     }
 }
